934e461e63d54da4040be105d3616ab68f3604fa
Antoine C <antoine.cozzucoli@gmail.com>
2021-06-28 18:18:34 +0200

    img modal added

--- src/assets/mock/students-mock.js
+++ src/assets/mock/students-mock.js
@@ -4,34 +4,34 @@ export const studentMock = [
         firstname: 'Paul',
         lastname: 'Dubois', 
         birthdate: new Date(1999, 8, 21), 
-        img: 'avatar_08'
+        img: 'avatar_08.png'
     }, 
     {
         id: 2,
         firstname: 'Laura',
         lastname: 'Deschamps', 
         birthdate: new Date(2000, 6, 1), 
-        img: 'avatar_09'
+        img: 'avatar_09.png'
     }, 
     {
         id: 3,
         firstname: 'Clara',
         lastname: 'Martin', 
         birthdate: new Date(1999, 2, 15), 
-        img: 'avatar_07'
+        img: 'avatar_07.png'
     }, 
     {
         id: 4,
         firstname: 'Pierre',
         lastname: 'Dupont', 
         birthdate: new Date(2001, 9, 8), 
-        img: 'avatar_12'
+        img: 'avatar_12.png'
     }, 
     {
         id: 5,
         firstname: 'Margaux',
         lastname: 'Roger', 
         birthdate: new Date(1999, 2, 15), 
-        img: 'avatar_14'
+        img: 'avatar_14.png'
     }
 ]
--- src/component/student-modal/student-modal.js
+++ src/component/student-modal/student-modal.js
@@ -4,38 +4,37 @@ import ReactModal from "react-modal";
 import { StudentForm } from "../student-form/student-form";
 import "./student-modal.css";
 import 'moment/locale/fr';
+import { getImgUrl } from "../../utils/img-url-helper";
 
 export function StudentModal(props) {
     const student = props.student;
     const [isEditing, setEditing] = useState(false);
     return (
-        <Fragment>
-            <ReactModal isOpen={student != null} ariaHideApp={false}>
-                <button onClick={props.onClose}>x</button>
-                <img src={student.img}/>
+        <ReactModal isOpen={student != null} ariaHideApp={false} shouldCloseOnOverlayClick={true} onRequestClose={props.onClose}>
+            <button onClick={props.onClose}>x</button>
+            <img className="student-img" alt="Photographie de l'élève" src={getImgUrl(student.img)} />
 
-                {
-                    isEditing ?
-                        <StudentForm student={student} onEditStudent={(student) => props.onEditStudent(student)} />
-                        :
+            {
+                isEditing ?
+                    <StudentForm student={student} onEditStudent={(student) => props.onEditStudent(student)} />
+                    :
+                    <div>
                         <div>
-                            <div>
-                                Prénom : {student.firstname}
-                            </div>
-                            <div>
-                                Nom : {student.lastname}
-                            </div>
-                            <div>
-                                Né(e) le : {moment(student.birthdate).locale('fr').format("Do MMM YYYY")}
-                            </div>
-
+                            Prénom : {student.firstname}
+                        </div>
+                        <div>
+                            Nom : {student.lastname}
+                        </div>
+                        <div>
+                            Né(e) le : {moment(student.birthdate).locale('fr').format("Do MMM YYYY")}
                         </div>
 
-                }
+                    </div>
 
-                <button onClick={() => props.onDeleteStudent(student.id)}>Supprimer l'évève</button>
-                <button onClick={() => setEditing(!isEditing)}>{isEditing ? 'Annuler' : 'Modifier l\'élève'}</button>
-            </ReactModal>
-        </Fragment>
+            }
+
+            <button onClick={() => props.onDeleteStudent(student.id)}>Supprimer l'évève</button>
+            <button onClick={() => setEditing(!isEditing)}>{isEditing ? 'Annuler' : 'Modifier l\'élève'}</button>
+        </ReactModal>
     )
 }
